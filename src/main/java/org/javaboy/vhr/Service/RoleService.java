1a367b86ff4f06e90ac997a20c645dd69d75cec0
Liutx <15666882775@163.com>
2020-05-12 20:42:04 +0800

    up

---
+++ src/main/java/org/javaboy/vhr/Service/RoleService.java
@@ -0,0 +1,18 @@
+package org.javaboy.vhr.Service;
+
+import org.javaboy.vhr.mapper.RoleMapper;
+import org.javaboy.vhr.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleService {
+    @Autowired
+    RoleMapper roleMapper;
+
+    public List<Role> getAllRoles(){
+        return roleMapper.getAllRoles();
+    }
+}
