9ae39577d27e44bd29017f69021719b823b4d77c
Liutx <15666882775@163.com>
2020-05-23 16:57:44 +0800

    存储过程实现删除部门树接口

--- src/main/java/org/javaboy/vhr/Service/DepartmentService.java
+++ src/main/java/org/javaboy/vhr/Service/DepartmentService.java
@@ -30,4 +30,8 @@ public class DepartmentService {
         dep.setEnabled(true);
         departmentMapper.addDep(dep);
     }
+
+    public void deleteDepById(Department dep) {
+        departmentMapper.deleteDepById(dep);
+    }
 }
--- src/main/java/org/javaboy/vhr/controller/system/basic/DepartmentController.java
+++ src/main/java/org/javaboy/vhr/controller/system/basic/DepartmentController.java
@@ -6,6 +6,7 @@ import org.javaboy.vhr.model.RespBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.RespectBinding;
 import java.util.List;
 
 /**
@@ -31,6 +32,21 @@ public class DepartmentController {
         }else {
             return RespBean.error("添加失败",dep);
         }
+    }
+
+    @DeleteMapping("/{id}")
+    public RespBean deleteDep(@PathVariable Integer id){
+        Department dep = new Department();
+        dep.setId(id);
+        departmentService.deleteDepById(dep);
+        if (dep.getResult() == -2){
+            return RespBean.error("该部门下有子部门，删除失败");
+        }else if (dep.getResult() == -1){
+            return RespBean.error("该部门下有员工");
+        }else if (dep.getResult() == -1){
+            return RespBean.ok("删除成功");
+        }
+        return RespBean.error("删除失败");
     }
 
 }
